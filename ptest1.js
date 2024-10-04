0aebaea71b9ec592af8f449c68be5e315cfc9609
Mikethenike <105406504+Mikethenike@users.noreply.github.com>
2024-10-03 20:30:30 -0500

    Add files via upload

---
+++ ptest1.js
@@ -0,0 +1,39 @@
+var previous = 1;
+function randomizer() {
+    function up() {
+        document.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowUp', code: 'ArrowUp', charCode: 38, keyCode: 38, bubbles: true }));
+        document.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowUp', code: 'ArrowUp', charCode: 38, keyCode: 38, bubbles: true }));
+    }
+    function right() {
+        document.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowRight', code: 'ArrowRight', charCode: 39, keyCode: 39, bubbles: true }));
+        document.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowRight', code: 'ArrowRight', charCode: 39, keyCode: 39, bubbles: true }));
+    }
+    function down() {
+        document.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter', code: 'Enter', charCode: 13, keyCode: 13, bubbles: true }));
+        document.dispatchEvent(new KeyboardEvent('keyup', { key: 'Enter', code: 'Enter', charCode: 13, keyCode: 13, bubbles: true }));
+    }
+    function left() {
+        document.dispatchEvent(new KeyboardEvent('keydown', { key: 'ArrowLeft', code: 'ArrowLeft', charCode: 37, keyCode: 37, bubbles: true }));
+        document.dispatchEvent(new KeyboardEvent('keyup', { key: 'ArrowLeft', code: 'ArrowLeft', charCode: 37, keyCode: 37, bubbles: true }));
+    }
+    function enter() {
+        document.dispatchEvent(new KeyboardEvent('keydown', { key: 'Enter', code: 'Enter', charCode: 13, keyCode: 13, bubbles: true }));
+        document.dispatchEvent(new KeyboardEvent('keyup', { key: 'Enter', code: 'Enter', charCode: 13, keyCode: 13, bubbles: true }));
+    }
+    var button = Math.random() * 2;
+    if (Math.ceil(button) == 1) {
+        enter();
+        previous = 1;
+    } else if (Math.ceil(button) == 2) {
+        if (previous == 1) {
+            right();
+        }
+        enter();
+        up();
+        for (var i = 0; i < 5; i++) {
+            up();
+            enter();
+        }
+        previous = 2;
+    }
+}
